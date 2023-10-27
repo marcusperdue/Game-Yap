2afd08c516212c55a2216690474b72a012a5f896
sefufim <132181028+sefufim@users.noreply.github.com>
2023-10-26 21:09:33 -0700

    filled out server.js and connections.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const sequelize = require('./config/connections');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const hbs = require("hbs");
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'process.env.SESSION_SECRET', // Change this to a secret key
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(session(sess));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
