494495c00f1b743863f3ab1a4360f48fceb95bfc
Marcus Perdue <marcusperduee@gmail.com>
2023-11-04 21:31:30 -0700

    fixed post route in registration

--- controllers/registrationRoutes.js
+++ controllers/registrationRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
 
-router.post('/register', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const { username, password } = req.body;
 
